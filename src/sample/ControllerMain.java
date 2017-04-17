1d1dda958ef578dffb00225c0614c9fa1fd40e0c
Grov12 <robinforsman@hotmail.com>
2017-04-17 16:24:09 +0200

    Added so that user have to fill in textfields if he wants to login as coach or player

--- src/sample/ControllerMain.java
+++ src/sample/ControllerMain.java
@@ -7,10 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -28,8 +25,6 @@ public class ControllerMain implements Initializable {
     private TextField textPassword;
 
 
-
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         choiceBox.getItems().add("Guest");
@@ -37,80 +32,125 @@ public class ControllerMain implements Initializable {
         choiceBox.getItems().add("Player");
 
 
-
     }
 
     @FXML
     private void loginButton(ActionEvent ae) {
+
         if (choiceBox.getValue().equals("Coach")) {
-            Node node = (Node) ae.getSource();
-            Stage stage = (Stage) node.getScene().getWindow();
-
-            FXMLLoader loader = new FXMLLoader(this.getClass().getResource("CoachScene.fxml"));
-
-            Parent root = null;
-
             try {
-                root = (Parent) loader.load();
-            } catch (IOException var7) {
-                var7.printStackTrace();
-            }
-
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-        }
-        else if (choiceBox.getValue().equals("Player")) {
-            Node node = (Node) ae.getSource();
-            Stage stage = (Stage) node.getScene().getWindow();
-
-            FXMLLoader loader = new FXMLLoader(this.getClass().getResource("PlayerScene.fxml"));
-
-            Parent root = null;
-
-            try {
-                root = (Parent) loader.load();
-            } catch (IOException var7) {
-                var7.printStackTrace();
-            }
-
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-        }
-
-      else if (choiceBox.getValue().equals("Guest")) {
+                throwExceptions();
                 Node node = (Node) ae.getSource();
                 Stage stage = (Stage) node.getScene().getWindow();
-
-                FXMLLoader loader = new FXMLLoader(this.getClass().getResource("GuestScene.fxml"));
-
+                FXMLLoader loader = new FXMLLoader(this.getClass().getResource("CoachScene.fxml"));
                 Parent root = null;
-
-                try {
-                    root = (Parent) loader.load();
-                } catch (IOException var7) {
-                    var7.printStackTrace();
-                }
-
+                root = (Parent) loader.load();
                 Scene scene = new Scene(root);
                 stage.setScene(scene);
-            }
-        }
-        public void handleMenuItemEdit() {
-            if(!textUsername.getText().isEmpty() || !textPassword.getText().isEmpty() || !choiceBox.getValue().equals(null)) {
-                textUsername.clear(); textPassword.clear();
-                choiceBox.setValue(null);
+            } catch (IOException var7) {
+                var7.printStackTrace();
+            } catch (NullPointerException e) {
+                Alert dialog = new Alert(Alert.AlertType.INFORMATION);
+                dialog.setTitle("Error");
+                dialog.setHeaderText("Error:");
+                dialog.setContentText("Both fields must be filled in!");
+                dialog.showAndWait();
 
             }
 
 
+
+
+
+        } else if (choiceBox.getValue().equals("Player")) {
+            try {
+                throwExceptions();
+                Node node = (Node) ae.getSource();
+                Stage stage = (Stage) node.getScene().getWindow();
+                FXMLLoader loader = new FXMLLoader(this.getClass().getResource("PlayerScene.fxml"));
+                Parent root = null;
+                root = (Parent) loader.load();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+
+
+            } catch (IOException var7) {
+                var7.printStackTrace();
+            } catch (NullPointerException e) {
+                Alert dialog = new Alert(Alert.AlertType.INFORMATION);
+                dialog.setTitle("Error");
+                dialog.setHeaderText("Error:");
+                dialog.setContentText("Both fields must be filled in!");
+                dialog.showAndWait();
+            } catch (Exception e) {
+               e.printStackTrace();
+            }
+
+
+
+
+
+
+
+
+        }  if (choiceBox.getValue().equals("Guest")) {
+            Node node = (Node) ae.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+
+            FXMLLoader loader = new FXMLLoader(this.getClass().getResource("GuestScene.fxml"));
+
+            Parent root = null;
+
+            try {
+                root = (Parent) loader.load();
+            } catch (IOException var7) {
+                var7.printStackTrace();
+            }catch (NullPointerException e) {
+                System.out.println("hi");
+            }
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        }
+    }
+
+    public void handleMenuItemEdit() {
+        if (!textUsername.getText().isEmpty() || !textPassword.getText().isEmpty() || !choiceBox.getValue().equals(null)) {
+            textUsername.clear();
+            textPassword.clear();
+            choiceBox.setValue(null);
+
         }
 
 
-    public void viewPlayers() {}
-    public void viewTeam() {}
-    public void viewStatistics() {}
-    public void viewMatches() {}
-}
+    }
+
+
+    public void viewPlayers() {
+    }
+
+    public void viewTeam() {
+    }
+
+    public void viewStatistics() {
+    }
+
+    public void viewMatches() {
+    }
+
+    @FXML
+    public void throwExceptions() {
+        if (textPassword.getText().isEmpty() && textUsername.getText().isEmpty()) {
+            throw new NullPointerException("Not possible");
+        }
+
+
+    }
+
+
+    }
+
+
 
 
 
